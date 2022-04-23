5bd2d54293cf88a8a9265915ca45cd11f5b0b9b3
Santa Fe Kiler <47928218+shyawkwardnoodle@users.noreply.github.com>
2022-04-22 18:49:42 -0700

    added spinning sprite to test github pushes
    not an actual push just speeen

---
+++ Assets/speeeen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speeeen : MonoBehaviour {
+    public float angle;
+    public float spinSpd;
+
+    // Start is called before the first frame update
+    void Start() {
+        angle = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        angle += spinSpd * Time.deltaTime;
+        angle -= 360 * Mathf.Floor(angle/360);
+
+        transform.eulerAngles = new Vector3(0,0,angle);
+    }
+}
